4497e9f14f4ac214c2b38914f870ad14115e13a3
kompilator69 <marcinorz@o2.pl>
2020-05-16 15:32:29 +0200

    Add project files.

---
+++ PS11/Util/CustomPageFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS11.Util
+{
+    public class CustomPageFilter : IPageFilter
+    {
+        private string companyName;
+        
+        public CustomPageFilter(IConfiguration config)
+        {
+            
+            companyName = config.GetValue<string>("CompanyName");
+        }
+        public void OnPageHandlerSelected(PageHandlerSelectedContext pageContext)
+        {
+            int a = 0;
+
+        }
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext pageContext)
+        {
+            int a = 0;
+        }
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext pageContext)
+        {
+            var result = pageContext.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["CompanyName"] = companyName;
+
+            }
+        }
+    }
+}
